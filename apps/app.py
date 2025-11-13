a8e035f59afe20f9f0454f78bf5ec58bd07d5935
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-11-13 10:50:56 +0900

    add mail-functon（長谷川）

--- apps/app.py
+++ apps/app.py
@@ -1,8 +1,14 @@
 from flask import Flask, request
 from flask_cors import CORS
 
+### メール関連
+from flask_mail import Mail
+import os
+###
+
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
+
 CORS(app)
 
 class Dammy_user:
@@ -25,3 +31,15 @@ def authentication_at_email_address():
         return f"Hello, {email}!"
     else:
         return "Hello, Guest!"
+    
+
+### メール機能
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+mail = Mail(app)
+###
