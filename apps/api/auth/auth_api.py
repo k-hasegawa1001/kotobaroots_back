ddbec303f540b506cd45d3b1745ccf777ecba19c
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-11-17 11:40:49 +0900

    auth_api.py テスト用で一旦コミット（長谷川）

--- apps/api/auth/auth_api.py
+++ apps/api/auth/auth_api.py
@@ -1,7 +1,7 @@
 import datetime
 # import logging
 
-from flask import Blueprint, request, render_template, current_app
+from flask import Blueprint, request, render_template, current_app, jsonify
 
 # メール
 from ...email import send_email
@@ -29,9 +29,6 @@ def index():
         send_email(email,"test","/test_mail",admin_name="名無しの専門学生")
         # #
 
-
-        ############# ここで2要素認証としてメールを送信する
-
         return f"Hello, {email}!"
     else:
         return "Hello, Guest!"
@@ -52,7 +49,49 @@ def login():
         password = login_data.get("password")
 
         user = User.query.filter_by(email=email).first()
+        # passwordが合っているかの確認（check_password）
+        if user.check_password(user.hashed_password, password):
+            # passwordが正しい場合
+            print('パスワード合ってる')
+
+        else:
+            # パスワードが間違っている場合
+            print('パスワード間違い')
+        
+        ############# user != nullなら、ここで2要素認証としてメールを送信する
+        
         print(login_data)
+        response = jsonify(login_data)
+        return response, 200
     except Exception as e:
         print(e)
 
+# @api.route("/verify-2fa/<email>")
+# def verify_2fa():
+#     print()
+
+@api.route("/create_user", methods=["POST"])
+def create_user():
+    current_app.logger.info("create_user-APIにアクセスがありました")
+    """
+    request.body(json)
+    {
+        "username": "...",
+        "email": "...",
+        "password": "..."
+    }
+    """
+    try:
+        create_user_data = request.get_json()
+        username = create_user_data.get("username")
+        email = create_user_data.get("email")
+        password = create_user_data.get("password")
+
+        ### セキュリティ面考慮（後で実装）
+        ###
+
+        new_user = User(username=username, email=email)
+        new_user.password=password # ここでハッシュ化が呼び出されてるはず
+
+    except Exception as e:
+        print(e)
