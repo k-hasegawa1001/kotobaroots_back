ed797a7abe2c91948e817909fb6f22702d7fd740
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-11-18 10:46:32 +0900

    created levels table（長谷川）

--- apps/api/kotobaroots/models.py
+++ apps/api/kotobaroots/models.py
@@ -7,3 +7,10 @@ class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     content = db.Column(db.String, nullable=False)
+
+### Level（学習レベル）
+class Level(db.Model):
+    __tablename__ = "levels"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    level_tag = db.Column(db.String, nullable=False)
